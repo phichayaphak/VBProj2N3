089b359b2102167656f3731ebc38562017ff8ab9
Or_Phich <phichayaphak.ph@gmail.com>
2019-02-07 11:10:16 +0700

    Update 7 กพ. 62

---
+++ Train61VB2N3/Train61VB2N3/frmSelectDB.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelectDB
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.btnShowProduct = New System.Windows.Forms.Button()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.btnShowSale = New System.Windows.Forms.Button()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnShowProduct
+        '
+        Me.btnShowProduct.Location = New System.Drawing.Point(33, 36)
+        Me.btnShowProduct.Name = "btnShowProduct"
+        Me.btnShowProduct.Size = New System.Drawing.Size(113, 34)
+        Me.btnShowProduct.TabIndex = 0
+        Me.btnShowProduct.Text = "Show Product"
+        Me.btnShowProduct.UseVisualStyleBackColor = True
+        '
+        'dgvData
+        '
+        Me.dgvData.AllowUserToAddRows = False
+        Me.dgvData.AllowUserToDeleteRows = False
+        Me.dgvData.AllowUserToResizeColumns = False
+        Me.dgvData.AllowUserToResizeRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.dgvData.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(33, 76)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.ReadOnly = True
+        Me.dgvData.RowHeadersVisible = False
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.dgvData.RowsDefaultCellStyle = DataGridViewCellStyle2
+        Me.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvData.Size = New System.Drawing.Size(655, 323)
+        Me.dgvData.TabIndex = 1
+        '
+        'btnShowSale
+        '
+        Me.btnShowSale.Location = New System.Drawing.Point(152, 36)
+        Me.btnShowSale.Name = "btnShowSale"
+        Me.btnShowSale.Size = New System.Drawing.Size(117, 34)
+        Me.btnShowSale.TabIndex = 2
+        Me.btnShowSale.Text = "Show Sale"
+        Me.btnShowSale.UseVisualStyleBackColor = True
+        '
+        'frmSelectDB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(718, 468)
+        Me.Controls.Add(Me.btnShowSale)
+        Me.Controls.Add(Me.dgvData)
+        Me.Controls.Add(Me.btnShowProduct)
+        Me.Name = "frmSelectDB"
+        Me.Text = "frmSelectDB"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnShowProduct As System.Windows.Forms.Button
+    Friend WithEvents dgvData As System.Windows.Forms.DataGridView
+    Friend WithEvents btnShowSale As System.Windows.Forms.Button
+End Class
---
+++ Train61VB2N3/Train61VB2N3/frmSelectDB.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class frmSelectDB
+    Dim strCon As String =
+        ConfigurationManager.ConnectionStrings("Train61VB2N3.My.MySettings.strCon").ConnectionString
+    Dim myCon As New SqlConnection
+    Dim myDA As New SqlDataAdapter
+    Dim myDS As New DataSet
+    Dim strSQL As String
+
+    Private Sub connetDB()
+        If myCon.State = ConnectionState.Open Then
+            myCon.Close()
+        End If
+        myCon.ConnectionString = strCon
+        myCon.Open()
+    End Sub
+
+    Private Sub btnShowProduct_Click(sender As Object, e As EventArgs) Handles btnShowProduct.Click
+        connetDB()
+        strSQL = "Select ProductID, ProductName,  UnitPrice, UnitsInStock, (UnitPrice * UnitsInStock) AS Total From Products "
+        myDA = New SqlDataAdapter(strSQL, myCon)
+        myDS.Clear()
+        myDA.Fill(myDS, "myCat")
+        dgvData.DataSource = myDS.Tables("myCat")
+
+        dgvData.Columns(0).Width = 100
+        dgvData.Columns(1).Width = 250
+        dgvData.Columns(2).Width = 100
+        dgvData.Columns(3).Width = 80
+        dgvData.Columns(4).Width = 100
+        dgvData.Columns(0).HeaderText = "รหัส"
+        dgvData.Columns(1).HeaderText = "ชื่อสินค้า"
+        dgvData.Columns(2).HeaderText = "ราคา"
+        dgvData.Columns(3).HeaderText = "คงเหลือ"
+        dgvData.Columns(4).HeaderText = "มูลค่า"
+
+    End Sub
+
+    Private Sub btnShowSale_Click(sender As Object, e As EventArgs) Handles btnShowSale.Click
+        connetDB()
+        strSQL = "Select OrderID, OrderDate, CompanyName, FirstName, Lastname From Orders, Customers, Employees Where Orders.CustomerID = Customers.CustomerID and Orders.EmployeeID = Employees.EmployeeID "
+        myDA = New SqlDataAdapter(strSQL, myCon)
+        myDS.Clear()
+        myDA.Fill(myDS, "myOrders")
+        dgvData.DataSource = myDS.Tables("myOrders")
+    End Sub
+End Class
